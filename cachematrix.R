a4a09a3ca9ab25ae7a66808aa87d0e10e21456e7
Martin Aristizabal <mearistizabal@gmail.com>
2015-03-22 08:32:41 -0500

    Assignment submitted with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+# First I have to say I don't feel very proud of this assignment as it was hard for me to
+#understand details of the working function. I had to "paraphrase" the sample and use much external
+#help. That said, let's go to the function!.
+#This function Sets a Gets the value of the matrix, and makes it available from cache in order 
+#to avoid to make this costly action repetitive.
+
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -11,6 +17,8 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
+
+#This function checks if the solved matrix is in caché and retuns it. Otherwise solves it.
 
 cacheSolve <- function(x=matrix(), ...) {
   m<-x$getmatrix()
